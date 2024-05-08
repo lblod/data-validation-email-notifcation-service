c87173463ad1dc31cb697f66c8214e6cb80e0db9
YJouhar47 <youssef.jouhar1998@hotmail.com>
2024-05-08 09:51:56 +0200

    sending email every 2 weeks after 6 months

--- app.js
+++ app.js
@@ -3,43 +3,51 @@ import { querySudo as query } from "@lblod/mu-auth-sudo";
 import { CronJob } from "cron";
 
 function getDateSixMonthsAgo() {
-  const now = new Date();
-  const sixMonthsAgo = new Date(now.setMonth(now.getMonth() - 6));
-  return sixMonthsAgo.toISOString();
+ const now = new Date();
+ const sixMonthsAgo = new Date(now.setMonth(now.getMonth() - 6));
+ return sixMonthsAgo.toISOString();
 }
 
 async function sendEmail(email, id) {
-  console.log(`Sending email to: ${email} with UUID: ${id}`);
-  await query(`           
+ console.log(`Sending email to: ${email} with UUID: ${id}`);
+ await query(`           
  PREFIX nmo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nmo#>
  PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
  PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
  INSERT DATA {
-   GRAPH <http://mu.semte.ch/graphs/system/email> {
-     <http://data.lblod.info/id/emails/${uuid()}> a nmo:Email;
-         nmo:messageFrom "noreply@vlaanderen.be";
-         nmo:emailTo "${email}";
-         nmo:messageSubject "Gegevens bijwerken";
-         nmo:plainTextMessageContent """
+     GRAPH <http://mu.semte.ch/graphs/system/email> {
+       <http://data.lblod.info/id/emails/${uuid()}> a nmo:Email;
+               nmo:messageFrom "noreply@vlaanderen.be";
+               nmo:emailTo "${email}";
+               nmo:messageSubject "Gegevens bijwerken";
+               nmo:plainTextMessageContent """
 
-          Je ontvangt deze e-mail als gebruiker van de module Contactgegevens in Loket voor Lokale Besturen. 
+                Je ontvangt deze e-mail als gebruiker van de module Contactgegevens in Loket voor Lokale Besturen. 
 
-          De contactgegevens van deze vestiging van jouw lokaal bestuur zijn 6 maanden geleden voor het laastst aangepast of gecontroleerd. 
-          
-          https://contactgegevens.lokaalbestuur.vlaanderen.be/vestigingen/${id}.
+                De contactgegevens van deze vestiging van jouw lokaal bestuur zijn 6 maanden geleden voor het laastst aangepast of gecontroleerd. 
+                
+                https://contactgegevens.lokaalbestuur.vlaanderen.be/vestigingen/${id}.
 
-          Wij verzoeken je de gegevens te bekijken en indien nodig aan te passen. 
-          Het is belangrijk dat je deze gegevens controleert en bijwerkt, aangezien ze worden gebruikt in andere toepassingen van de Vlaamse overheid. 
-          Heb je nog vragen? Aarzel dan niet om ons per e-mail te contacteren op LoketLokaalBestuur@vlaanderen.be.        
-         """;
-         nmo:isPartOf <http://data.lblod.info/id/mail-folders/2>.
-   }
+                Wij verzoeken je de gegevens te bekijken en indien nodig aan te passen. 
+                Het is belangrijk dat je deze gegevens controleert en bijwerkt, aangezien ze worden gebruikt in andere toepassingen van de Vlaamse overheid. 
+                Heb je nog vragen? Aarzel dan niet om ons per e-mail te contacteren op LoketLokaalBestuur@vlaanderen.be.        
+               """;
+               nmo:isPartOf <http://data.lblod.info/id/mail-folders/2>.
+     }
  }`);
 }
 
-const job = new CronJob("* * * * *", async function () {
-  console.log("Running a task every minute");
-  try {
+let emailSent = false; 
+
+const job = new CronJob("0 0 */2 * *", async function () {
+ console.log("Running a task every two weeks");
+ try {
+    if (emailSent) {
+      console.log("Email already sent. Stopping the job.");
+      this.stop(); 
+      return;
+    }
+
     const dateString = getDateSixMonthsAgo();
     const results = await query(`
         PREFIX org: <http://www.w3.org/ns/org#>
@@ -72,9 +80,10 @@ const job = new CronJob("* * * * *", async function () {
       const uuid = binding.uuid.value;
       await sendEmail(email, uuid);
     }
-  } catch (error) {
+
+    emailSent = true;
     console.error(error);
-  }
+ }
 });
 
 job.start();
