576526bf34fc7ae6b7fae29b1db9286fe7cad4f9
YJouhar47 <youssef.jouhar1998@hotmail.com>
2024-06-28 13:07:00 +0200

    added feedback of Laurent

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ import { uuid } from "mu";
 import { querySudo as query } from "@lblod/mu-auth-sudo";
 import { CronJob } from "cron";
 
-function getDateMonthsAgo() {
+const DEFAULT_NUMBER_OF_MONTHS = parseInt(process.env.NUMBER_OF_MONTHS) || 6;
+
+if (DEFAULT_NUMBER_OF_MONTHS === 6) {
+  console.warn('WARNING: NUMBER_OF_MONTHS is not set. Using default value.');
+}
+const NumberOfMonths = DEFAULT_NUMBER_OF_MONTHS;
+
+function getDateMonthsAgo(monthsAgo) {
   const now = new Date();
-  const sixMonthsAgo = new Date(
-    now.setMonth(now.getMonth() - parseInt(process.env.NUMBER_OF_MONTHS) || 6),
-  );
-  return sixMonthsAgo.toISOString();
+  const targetDate = new Date(now.setMonth(now.getMonth() - monthsAgo));
+  return targetDate.toISOString();
 }
 
 async function sendEmail(email, id) {
+  const emailUri = `http://data.lblod.info/id/emails/${uuid()}`;
   console.log(`Sending email to: ${email} with UUID: ${id}`);
   await query(`           
  PREFIX nmo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nmo#>
@@ -18,7 +24,7 @@ async function sendEmail(email, id) {
  PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
  INSERT DATA {
      GRAPH <http://mu.semte.ch/graphs/system/email> {
-       <http://data.lblod.info/id/emails/${uuid()}> a nmo:Email;
+       <${emailUri}> a nmo:Email;
                nmo:messageFrom "noreply@vlaanderen.be";
                nmo:emailTo "${email}";
                nmo:messageSubject "Contact- en organisatiegegevens corrigeren/bevestigen";
@@ -26,9 +32,7 @@ async function sendEmail(email, id) {
 
                Je ontvangt deze e-mail als gebruiker van de module Contact- en Organisatiegegevens in Loket voor Lokale Besturen. 
                 
-                De contactgegevens van deze vestiging van jouw organisatie zijn ${
-                  process.env.NUMBER_OF_MONTHS
-                } maanden geleden voor het laatst aangepast of bevestigd.
+                De contactgegevens van deze vestiging van jouw organisatie zijn ${NumberOfMonths} maanden geleden voor het laatst aangepast of bevestigd.
 
                 ${process.env.URL}/vestigingen/${id}.
 
@@ -47,12 +51,12 @@ let lastEmailSentAt = null;
 const job = new CronJob(
   process.env.CRON_JOB || "0 0 */2 * *",
   async function () {
-    console.log("Running a task every two weeks");
-    console.log("Number Of Months", process.env.NUMBER_OF_MONTHS);
+    console.log("Running a task every");
+    console.log("Number Of Months", NumberOfMonths);
     console.log("Cron Job", process.env.CRON_JOB);
 
     try {
-      const dateString = getDateMonthsAgo();
+      const dateString = getDateMonthsAgo(6);
       const results = await query(`
         PREFIX org: <http://www.w3.org/ns/org#>
         PREFIX schema: <http://schema.org/>
@@ -77,21 +81,12 @@ const job = new CronJob(
             }
             FILTER((?modifiedPrimarySite < "${dateString}"^^xsd:dateTime ) || (?modified < "${dateString}"^^xsd:dateTime))
         }
-            `);
-
-      const now = new Date();
-      if (
-        !lastEmailSentAt ||
-        now.getTime() - lastEmailSentAt.getTime() >=
-          6 * 30 * 24 * 60 * 60 * 1000
-      ) {
+      `);
         for (const binding of results.results.bindings) {
           const email = binding.email.value;
           const uuid = binding.uuid.value;
           await sendEmail(email, uuid);
         }
-        lastEmailSentAt = now;
-      }
     } catch (error) {
       console.error(error);
     }
